4483ab0108033147a23e95d0c5063f5b403e92d9
vmlayton87 <vmlayton87@gmail.com>
2024-02-08 19:17:04 -0500

    first commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
+
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
